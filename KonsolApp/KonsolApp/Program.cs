6213821bfe938fab2a196cb259b2c5f272257402
Arian0240 <128600124+ArianYN@users.noreply.github.com>
2024-01-15 11:01:24 +0100

    "Display the following:"

--- KonsolApp/KonsolApp/Program.cs
+++ KonsolApp/KonsolApp/Program.cs
@@ -1,4 +1,12 @@
-﻿Console.Write("Congratulations!");
+﻿// Write your first code
+Console.Write("Congratulations!");
 Console.Write(" ");
 Console.Write("You wrote your first lines of code");
 Console.ReadLine();
+
+//Write code to display the following output: 
+// This is the first line.
+// This is the second line.
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
+
