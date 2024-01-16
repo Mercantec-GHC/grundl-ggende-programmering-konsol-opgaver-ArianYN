41b676305b8c2ade2cac85ff79dcda15a7d58213
Arian0240 <128600124+ArianYN@users.noreply.github.com>
2024-01-16 12:13:59 +0100

    Inventory Array

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -109,21 +109,33 @@ Random dice = new Random();
 // fraudulentOrderIDs[3] = "D000";
 
 
-string[] fraudulentOrderIDs = {"A123", "B456", "C789"};
+// string[] fraudulentOrderIDs = {"A123", "B456", "C789"};
 
-Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
-Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
-Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+// Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+// Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
 
-fraudulentOrderIDs[0] = "F000";
-Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
-Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+// fraudulentOrderIDs[0] = "F000";
+// Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
 
-string[] names = {"Rowena", "Robin", "Bao"};
-foreach (string name in names)
+// string[] names = {"Rowena", "Robin", "Bao"};
+// foreach (string name in names)
+// {
+//     Console.WriteLine(name);
+// }
+
+int[] inventory = {200, 450, 700, 175, 250};
+
+int sum = 0;
+int bin = 0;
+foreach (int items in inventory)
 {
-    Console.WriteLine(name);
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} items (Running total: {sum})");
 }
+Console.WriteLine($"We have {sum} items in inventory");
 
 
 
--- KonsolApp/KonsolApp/obj/Debug/net8.0/KonsolApp.AssemblyInfo.cs
+++ KonsolApp/KonsolApp/obj/Debug/net8.0/KonsolApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KonsolApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8c881ad4131fa72c037ec7a4c53f751bd887085")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+534cc7297fec81bdb576736dc71ab58afe622fa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("KonsolApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KonsolApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
