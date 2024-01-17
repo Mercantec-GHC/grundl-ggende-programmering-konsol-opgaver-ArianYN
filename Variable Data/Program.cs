c69299fe2c994257659bd9b1cdb570891d4a86dc
Arian0240 <128600124+ArianYN@users.noreply.github.com>
2024-01-17 12:09:46 +0100

    Convert values to int and decimal

--- Variable Data/Program.cs
+++ Variable Data/Program.cs
@@ -73,26 +73,39 @@
 //if (result > 0)
 //    Console.WriteLine($"Measurement (w/ offset): {50 + result}");
 
-string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+//string[] values = { "12.3", "45", "ABC", "11", "DEF" };
 
-decimal number = 0m;
-string message = "";
+//decimal number = 0m;
+//string message = "";
 
-foreach (string value in values)
-{
-    decimal foundNumber;
-    if (decimal.TryParse(value, out foundNumber))
-    {
-        number += foundNumber;
-    }
-    else
-    {
-        message += value;
-    }
-}
+//foreach (string value in values)
+//{
+//    decimal foundNumber;
+//    if (decimal.TryParse(value, out foundNumber))
+//    {
+//        number += foundNumber;
+//    }
+//    else
+//    {
+//        message += value;
+//    }
+//}
 
-Console.WriteLine($"Message: {message}");
-Console.WriteLine($"Total: {number}");
+//Console.WriteLine($"Message: {message}");
+//Console.WriteLine($"Total: {number}");
+
+int value1 = 12;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+int result1 = value1 / (int)value2;
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
 
 
 Console.ReadLine();
