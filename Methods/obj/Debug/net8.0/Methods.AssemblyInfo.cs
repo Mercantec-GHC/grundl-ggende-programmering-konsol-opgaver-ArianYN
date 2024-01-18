074028d5758fa1aea98457fdad625ae9b0a3fde8
Arian0240 <128600124+ArianYN@users.noreply.github.com>
2024-01-18 08:55:00 +0100

    TellLuck Method

--- Methods/obj/Debug/net8.0/Methods.AssemblyInfo.cs
+++ Methods/obj/Debug/net8.0/Methods.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Methods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b13c8db43858cb06fa06257180438e30e52702e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3413e837c94f9186579e4ce459e4707a2f402d4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Methods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Methods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
